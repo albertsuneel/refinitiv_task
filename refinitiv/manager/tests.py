953cec971590dafad46985238136b8004aabda10
suneel <albertsuneel420@gmail.com>
2021-07-11 12:02:25 +0530

    Added unit tests

--- refinitiv/manager/tests.py
+++ refinitiv/manager/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from .models import Rates, Branches, Manager
 
-# Create your tests here.
+
+class RatesTestCase(TestCase):
+    def setUp(self):
+        Rates.objects.create(currency="USD", rate=30, spread=.20)
+        Rates.objects.create(currency="CAD", rate=23.83, spread=0.40)
+
+    def test_rates_model(self):
+        usd = Rates.objects.get(currency="USD")
+        cad = Rates.objects.get(currency="CAD")
+        self.assertEqual(usd.rate, 30)
+        self.assertEqual(cad.spread, 0.40)
+
+
+class BranchesTestCase(TestCase):
+    def setUp(self):
+        Branches.objects.create(branch_id="STW", name="Seoul Branch", address_line1="9F S Tower",
+                                address_line2="82 Saemunan-ro, Jongro-gu", address_line3="", city="Seoul",
+                                zip="03185", country="South Korea", tel="+82264545400")
+
+    def test_branches_model(self):
+        branch = Branches.objects.get(branch_id="STW")
+        self.assertEqual(branch.name, "Seoul Branch")
+        self.assertEqual(branch.country, "South Korea")
+        self.assertEqual(branch.zip, "03185")
+
+
+class ManagerTestCase(TestCase):
+    def setUp(self):
+        Manager.objects.create(name="Suneel", email="albertsuneel420@gmail.com", password="admin@123")
+
+    def test_manager_model(self):
+        manager = Manager.objects.get(email="albertsuneel420@gmail.com")
+        self.assertEqual(manager.name, "Suneel")
+        self.assertEqual(manager.password, "admin@123")
--- refinitiv/user/tests.py
+++ refinitiv/user/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from datetime import datetime
+from.models import User
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(transaction_id="sdsdss877s990s", timestamp=datetime.now(), name="Suneel", position="Buy", currency="USD",
+                            fx_rate_hq=30.1, fx_rate_spread=0.02, amount=5218,branch_id="UCL",  comments="---",
+                            status=1)
+
+    def test_user_model(self):
+        data = User.objects.get(transaction_id="sdsdss877s990s")
+        self.assertEqual(data.name, "Suneel")
+        self.assertEqual(data.branch_id, "UCL")
+        self.assertEqual(data.position, "Buy")
+        self.assertEqual(data.amount, str(5218))
+
